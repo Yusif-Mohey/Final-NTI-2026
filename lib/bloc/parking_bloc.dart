42ff7760ff0109e54cb83b5e4680f14cc751d090
Salma <salmamamdouh360@gmail.com>
2026-04-14 10:42:07 +0200

    add bloc

--- lib/bloc/parking_bloc.dart
+++ lib/bloc/parking_bloc.dart
@@ -1,10 +1,40 @@
+import 'dart:async';
 import 'package:bloc/bloc.dart';
 
 part 'parking_event.dart';
 part 'parking_state.dart';
 
 class ParkingBloc extends Bloc<ParkingEvent, ParkingState> {
+  Timer? timer;
+  int seconds = 0;
+  double pricePerMinute = 2;
+
   ParkingBloc() : super(ParkingInitialState()) {
-    on<StartParkingEvent>((event, emit) {});
+    on<StartParkingEvent>((event, emit) {
+      seconds = 0;
+
+      timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+        add(TickTimerEvent());
+      });
+    });
+
+    on<TickTimerEvent>((event, emit) {
+      seconds++;
+      emit(ParkingRunningState(seconds));
+    });
+
+    on<EndParkingEvent>((event, emit) {
+      timer?.cancel();
+
+      double minutes = seconds / 60;
+      double price = minutes * pricePerMinute;
+
+      emit(ParkingFinishedState(seconds, price));
+    });
+
+    on<ResetParkingEvent>((event, emit) {
+      seconds = 0;
+      emit(ParkingInitialState());
+    });
   }
 }
