331d51d80e0e67df63d5c588dd835f03a084cd28
mariam195 <hanymariam123456@gmail.com>
2026-04-21 13:00:23 +0200

    add bloc auth

--- lib/features/auth/presentation/bloc/auth_event.dart
+++ lib/features/auth/presentation/bloc/auth_event.dart
@@ -8,3 +8,12 @@ class LoginEvent extends AuthEvent {
 
   LoginEvent({required this.email, required this.password});
 }
+ 
+
+final class signUp extends AuthEvent {
+  final String email;
+  final String password;
+  final String Name;
+
+  signUp({required this.email, required this.password, required this.Name});
+}
